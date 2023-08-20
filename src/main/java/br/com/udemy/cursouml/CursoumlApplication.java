a69e313ea208cd76b3948fbc6ec84cebb18b4232
Caio Lucena <caio.lucena10@hotmail.com>
2023-08-20 16:22:44 -0300

    Criando operação de instanciação

--- src/main/java/br/com/udemy/cursouml/CursoumlApplication.java
+++ src/main/java/br/com/udemy/cursouml/CursoumlApplication.java
@@ -1,13 +1,32 @@
 package br.com.udemy.cursouml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.udemy.cursouml.domain.Categoria;
+import br.com.udemy.cursouml.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoumlApplication {
+public class CursoumlApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+
 	}
 
 }

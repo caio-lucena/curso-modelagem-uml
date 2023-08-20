8937f5f9ad0d32a6138a5fccdcbd85a320167612
Caio Lucena <caio.lucena10@hotmail.com>
2023-08-20 19:32:52 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/udemy/cursouml/CursoumlApplication.java
+++ src/main/java/br/com/udemy/cursouml/CursoumlApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.udemy.cursouml.domain.Categoria;
+import br.com.udemy.cursouml.domain.Produto;
 import br.com.udemy.cursouml.repositories.CategoriaRepository;
+import br.com.udemy.cursouml.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoumlApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
@@ -24,8 +29,20 @@ public class CursoumlApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		Produto produto1 = new Produto(null, "Computador", 2000.0);
+		Produto produto2 = new Produto(null, "Impressora", 800.0);
+		Produto produto3 = new Produto(null, "Mouse", 80.0);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
 
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 
 	}
 

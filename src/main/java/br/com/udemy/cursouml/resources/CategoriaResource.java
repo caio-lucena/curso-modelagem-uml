96d1aaf0d7b93d5d941c742ec0ca1a17b8f761e6
Caio Lucena <caio.lucena10@hotmail.com>
2023-08-20 10:16:11 -0300

    REST testado

---
+++ src/main/java/br/com/udemy/cursouml/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package br.com.udemy.cursouml.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Chamada REST está funcionando!";
+	}
+
+}

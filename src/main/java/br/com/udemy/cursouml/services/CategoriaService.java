be6dd51752c67722ece6ce7826788556b9d969a3
Caio Lucena <caio.lucena10@hotmail.com>
2023-08-25 08:18:55 -0300

    ajuste no endpoint /categorias/{id}

--- src/main/java/br/com/udemy/cursouml/services/CategoriaService.java
+++ src/main/java/br/com/udemy/cursouml/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.udemy.cursouml.domain.Categoria;
 import br.com.udemy.cursouml.repositories.CategoriaRepository;
+import br.com.udemy.cursouml.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {						
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Não foi possível encontrar o objeto ID: "
+				+ id + ", tipo: "+ Categoria.class.getName()));
 	}
 
 }

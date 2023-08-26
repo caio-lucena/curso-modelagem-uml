67da570c92423d1a05ddb482d2b9df63d928e919
Caio Lucena <caio.lucena10@hotmail.com>
2023-08-26 14:07:44 -0300

    Incluindo Estado e Cidade

--- src/main/java/br/com/udemy/cursouml/CursoumlApplication.java
+++ src/main/java/br/com/udemy/cursouml/CursoumlApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.udemy.cursouml.domain.Categoria;
+import br.com.udemy.cursouml.domain.Cidade;
+import br.com.udemy.cursouml.domain.Estado;
 import br.com.udemy.cursouml.domain.Produto;
 import br.com.udemy.cursouml.repositories.CategoriaRepository;
+import br.com.udemy.cursouml.repositories.CidadeRepository;
+import br.com.udemy.cursouml.repositories.EstadoRepository;
 import br.com.udemy.cursouml.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoumlApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
@@ -43,6 +53,22 @@ public class CursoumlApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
+		
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+		
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+		
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		
+		
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+		
+		
 
 	}
 
